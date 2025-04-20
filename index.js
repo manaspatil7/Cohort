a6e309f219116ced09d99aaf00a1738275b7f4e7
manaspatil7 <121172076+manaspatil7@users.noreply.github.com>
2025-04-20 12:09:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstname="john";
+const age="30";
+var isStudent=true;
+
+var isStudent=false;
+
+console.log(firstname);
+console.log(age);
+console.log(isStudent);
+let firsname="manas";
+console.log(firstname);
